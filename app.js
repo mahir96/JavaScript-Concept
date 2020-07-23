0321a63873de7f5829850a8a811d40173dd6d079
Mahir Juma <mahirdateflondon@gmail.com>
2020-07-23 12:48:56 +0300

    run fix

--- app.js
+++ app.js
@@ -1,62 +1,103 @@
 // General loops
-
-// for (let i = 0; i < 9; i++) {
+let num = 8;
+// for (let i = 0; i < num; i++) {
 //   console.log(i);
 // }
-// for (let i = 0; i < 10; i++) {
-//   if (i === 2) {
-//     console.log(`${i} is My favorit Number`);
+
+// for (let i = 0; i < num; i++) {
+//   if (i === 3) {
+//     console.log(`${i} is my favorit number`);
 //     continue;
 //   } else if (i === 6) {
 //     break;
 //   }
-//     console.log(i);
+//   console.log(i);
 // }
 
-// while loop
+// While loop
 // let i = 0;
-// while (i < 10) {
+// while (i < num) {
 //   console.log(i);
 //   i++;
 // }
 
-// Do while loop
-// let i = 0;
-// do{
-//     console.log(i);
-//     i++
-// }while(i <= 10)
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < num);
 
-// loop through Array
-const fruits = ["Apple", "Orange", "Graps"];
-// for (let i = 0; i < fruit.length; i++) {
-//   console.log(fruit[i]);
-// }
+// for each
+const fruits = ["Apple", "Graps", "Banana"];
 
-// forEach
 // fruits.forEach(function (fruit) {
 //   console.log(fruit);
 // });
 
-// Arrays of Object
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+// const myPush = fruits.map(function (fruit) {
+//   return fruit;
+// });
+// console.log(myPush.push("Orange"));
+// console.log(myPush);
 
-const users = [
-  { id: 1, name: "Dumilla" },
-  { id: 2, name: "Mahir" },
-  { id: 3, name: "Dullah" },
-];
-const ids = users.map(function (user) {
-  return user.id;
-});
-// console.log(ids);
+// const users = [
+//   { id: 1, name: "Mahir" },
+//   { id: 2, name: "Afla" },
+//   { id: 3, name: "Juma" },
+// ];
+// const ids = users.map(function (user) {
+//   return user.id;
+// });
 
-const person = {
-  firstName: "Mahir",
-  lastName: "Juma",
-  age: 25,
-};
+// console.log(typeof ids);
 
-// For In
-for (let x in person) {
-  console.log(`${x}: ${person[x]}`);
+const id = 100;
+
+// if (id == 100) {
+//   console.log("Correct");
+// } else {
+//   console.log("incorrect");
+// }
+
+// if (id !== 101) {
+//   console.log("correct");
+// } else {
+//   console.log("Incorrect");
+// }
+
+// Value and type
+// if (id === 100) {
+//   console.log("Correct");
+// } else {
+//   console.log("Incorrect");
+// }
+
+// if (id !== 100) {
+//   console.log("Correct");
+// } else {
+//   console.log("Incorrect");
+// }
+
+// if (typeof id !== "undefined") {
+//   console.log(`the id Is ${id}`);
+// } else {
+//   console.log("No id ");
+// }
+
+const name = "Mahir";
+const age = 17;
+// if (age > 0 && age < 12) {
+//   console.log(`${name} your a child`);
+// } else if (age >= 13 && age <= 19) {
+//   console.log("you an teenage");
+// } else {
+//   console.log("your an Adult");
+// }
+
+if (age < 15 || age > 30) {
+  console.log("you can not do an exam");
+} else {
+  console.log("you can do exam");
 }
